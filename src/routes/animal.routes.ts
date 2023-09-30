7d87c22031cb7e6aabd33e229e6a0812def2a673
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-09-30 00:49:47 -0500

    crea las validaciones basicas para guardar un nuevo animal

--- src/routes/animal.routes.ts
+++ src/routes/animal.routes.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
+import {  body } from "express-validator";
+
 import { getAnimals, postAnimals } from "../controllers/animal.controllers";
+import { validateFields } from "../middlewares/validateFields";
 
 const router = Router()
 
 router.get('/', getAnimals)
-router.post('/', postAnimals)
+
+// todo: validar que la fecha este en el formato yyyy-mm-dd
+// todo: validad que el identificador no se repita en la base de datos
+router.post('/', [
+  body('animalType', "El tipo de animal es requerido").notEmpty().isString(),
+  body("animalType", "El tipo de animal no es valido").if(body("animalType").exists()).isIn(["ganado"]),
+  body('identificationNumber', "El numero de identificacion del animal es requerido").notEmpty().isNumeric(),
+  body("race", "La raza del animal es requerida").notEmpty().isString(),
+  body("age", "La edad del animal es requerida").notEmpty(),
+  body("weight", "El peso del animal es requerido").notEmpty().isNumeric(),
+  body("weight", "El peso del animal no es valido").isFloat({ min: 0 }),
+  body("imagenUrl", "La imagen del animal es requerida").notEmpty().isString(),
+  body("imagenUrl", "La imagen del animal no es valida").if(body("imagenUrl").exists()).isURL(),
+  validateFields
+] ,postAnimals)
 
 export default router;
