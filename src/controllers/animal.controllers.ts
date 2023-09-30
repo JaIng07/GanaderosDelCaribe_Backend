d5da4726f900c19daa002a9a901e4b9b9cd15ec8
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-09-30 01:54:19 -0500

    crea el metodo para obtener todos los animales

--- src/controllers/animal.controllers.ts
+++ src/controllers/animal.controllers.ts
@@ -3,10 +3,39 @@ import { Request, Response } from "express";
 import AppDataSource from "../database/config";
 import { Animal } from "../entities/Animal.entities";
 
-export const getAnimals = (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "se obtuvo el animal"
-  })
+/**
+ * La función `getAnimals` obtiene todos los animales de una fuente de datos y los devuelve en orden
+ * inverso.
+ * @param {Request} req - El parámetro `req` es un objeto que representa la solicitud HTTP hecha al
+ * servidor. Contiene información como el método de la solicitud, encabezados, parámetros de consulta y cuerpo.
+ * @param {Response} res - El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta
+ * HTTP de vuelta al cliente. Contiene métodos y propiedades que le permiten establecer el código de estado de la
+ * respuesta, los encabezados y el cuerpo. En este caso, se está utilizando para enviar una respuesta JSON con un
+ * código de estado.
+ * @returns una respuesta JSON con un mensaje y un array de animales. Si la obtención de los animales es
+ * exitosa, el mensaje indicará que se han obtenido correctamente todos los animales y los animales
+ * se devolverán en orden inverso para obtener en el array al inicio los mas recientes.
+ * Si hay un error al obtener los animales, el mensaje indicará que ha ocurrido un error y
+ * el objeto de error se incluirá en la respuesta.
+ */
+export const getAnimals = async (req: Request, res: Response) => {
+
+  try {
+
+    const response = await AppDataSource.getRepository(Animal).find()
+
+    return res.status(200).json({
+      message: "Se han obtenido todos los animales correctamente",
+      animals: response.reverse()
+    })
+
+  } catch (error) {
+    return res.status(400).json({
+      message: "Ha ocurrido un error al obtener los animales",
+      error: error
+    })
+  }
+
 }
 
 /**
@@ -21,7 +50,7 @@ export const getAnimals = (req: Request, res: Response) => {
  * guarda correctamente. Si hay un error, devuelve una respuesta JSON con un mensaje de error y los
  * detalles del error.
  */
-export const postAnimals = async(req: Request, res: Response) => {
+export const postAnimals = async (req: Request, res: Response) => {
   const bodyData = req.body
   const { animalType, identificationNumber, race, birthdate, weight, imagenUrl } = bodyData
 
@@ -51,4 +80,3 @@ export const postAnimals = async(req: Request, res: Response) => {
   }
 
 }
-
